3d1a7c6112a65f95f6a16aa2040839e43e5f1c9d
Cheryn <112348692+CherynL@users.noreply.github.com>
2025-03-29 11:42:33 -0500

    Project upload part 2
    Added app/src but not yet app/build

---
+++ PlanIt/app/src/main/java/com/crypto_20_we_love_ads/planit/CalenderActivity.kt
@@ -0,0 +1,41 @@
+package com.crypto_20_we_love_ads.planit
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class CalenderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.calender_screen)
+
+
+
+        findViewById<View>(R.id.addEvent).setOnClickListener {
+            val intent = Intent(this, AddActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<View>(R.id.homeButton).setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<View>(R.id.scheduleButton).setOnClickListener {
+        }
+
+        findViewById<View>(R.id.searchButton).setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<View>(R.id.settingsButton).setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
